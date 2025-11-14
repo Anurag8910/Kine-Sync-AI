eb88cd8478731d38a23274b837ab4f867c73cec5
Anurag8910 <pundiranurag69@gmail.com>
2025-11-14 20:25:34 +0530

    fixed some bug

--- src/main.jsx
+++ src/main.jsx
@@ -6,8 +6,6 @@ import Dashboard from './dashboard/Dashboard.jsx'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    {/* <App /> */}
-    {/* <App/> */}
-    <Dashboard/>
+    <App /> 
   </StrictMode>,
 )
